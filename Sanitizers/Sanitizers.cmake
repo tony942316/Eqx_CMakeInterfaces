9c717dc169ee41323562ff6a3e404726c178e1f3
tony942316 <grassoa159@gmail.com>
2024-04-19 20:14:55 +0000

    Win Fix

--- Sanitizers/Sanitizers.cmake
+++ Sanitizers/Sanitizers.cmake
@@ -1,9 +1,13 @@
 cmake_minimum_required(VERSION 3.28)
 
-include("${CMAKE_CURRENT_LIST_DIR}/ASan/ASan.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/LSan/LSan.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/TSan/TSan.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/UBSan/UBSan.cmake")
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    include("${CMAKE_CURRENT_LIST_DIR}/ASan/ASan.cmake")
+else ()
+    include("${CMAKE_CURRENT_LIST_DIR}/ASan/ASan.cmake")
+    include("${CMAKE_CURRENT_LIST_DIR}/LSan/LSan.cmake")
+    include("${CMAKE_CURRENT_LIST_DIR}/TSan/TSan.cmake")
+    include("${CMAKE_CURRENT_LIST_DIR}/UBSan/UBSan.cmake")
+endif()
 
 add_library(EQX-Sanitizers INTERFACE)
 
