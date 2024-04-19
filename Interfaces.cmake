39db7fc230f63be6d3b3ef411a3a9cc43adf3288
tony942316 <grassoa159@gmail.com>
2024-04-19 20:08:50 +0000

    Added EQX-Typical

--- Interfaces.cmake
+++ Interfaces.cmake
@@ -4,3 +4,14 @@ include("${CMAKE_CURRENT_LIST_DIR}/DebugSymbols/DebugSymbols.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/Optimizations/Optimizations.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/Sanitizers/Sanitizers.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/Warnings/Warnings.cmake")
+
+add_library(EQX-Typical INTERFACE)
+target_link_libraries(EQX-Typical INTERFACE EQX-Warnings)
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    target_link_libraries(EQX-Typical INTERFACE
+        "$<$<CONFIG:Debug>:EQX-DebugSymbols;EQX-Sanitizers>"
+        "$<$<CONFIG:Release>:EQX-Optimizations>")
+else ()
+    target_link_libraries(EQX-Typical INTERFACE
+        EQX-DebugSymbols EQX-Sanitizers)
+endif()
