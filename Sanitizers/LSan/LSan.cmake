549b25d7bbaf01728828b8a49dd515595748eaa8
tony942316 <grassoa159@gmail.com>
2024-04-19 20:02:19 +0000

    Sanitizer Tweaks

--- Sanitizers/LSan/LSan.cmake
+++ Sanitizers/LSan/LSan.cmake
@@ -1,5 +1,9 @@
 cmake_minimum_required(VERSION 3.28)
 
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    message("MSVC Does Not Support Leak Sanitizer!!!")
+endif()
+
 add_library(EQX-LSan INTERFACE)
 target_compile_options(EQX-LSan INTERFACE -fsanitize=leak)
 target_link_options(EQX-LSan INTERFACE -fsanitize=leak)
