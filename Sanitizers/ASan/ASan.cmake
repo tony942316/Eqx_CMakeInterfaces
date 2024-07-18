858e8f9b36199dfd32f0550e93266a34574fa249
tony942316 <grassoa159@gmail.com>
2024-07-18 21:20:51 +0000

    Suppress Warning On Windows

--- Sanitizers/ASan/ASan.cmake
+++ Sanitizers/ASan/ASan.cmake
@@ -4,6 +4,7 @@ add_library(EQX-ASan INTERFACE)
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     target_compile_options(EQX-ASan INTERFACE /fsanitize=address)
+    target_link_options(EQX-ASan INTERFACE /INCREMENTAL:NO)
 else ()
     target_compile_options(EQX-ASan INTERFACE -fsanitize=address)
     target_link_options(EQX-ASan INTERFACE -fsanitize=address)
